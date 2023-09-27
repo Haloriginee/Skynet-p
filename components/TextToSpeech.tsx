a2873e9635f33bd6ad032e8c751fabda438d19ac
Kevin Hang <herezy2006@gmail.com>
2023-09-27 06:30:12 +0200

    cool stuff i guess

--- components/TextToSpeech.tsx
+++ components/TextToSpeech.tsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export const TextToSpeech = () => {
   return (
-    <div>TextToSpeech</div>
+    <div>
+      <form className= 'p-1 w-[510px] rounded-full absolute top-[800px] left-[30%] bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500'>
+        <input
+          type='text'
+          className='p-3 w-full rounded-full outline-none bg-black'
+          placeholder='Wingardium Patronum...'
+        />
+        <button></button>
+      </form>
+    </div>
   )
 }
