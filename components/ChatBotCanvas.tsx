684e834c670844420548cef0a628bfa4928e7286
Kevin Hang <herezy2006@gmail.com>
2023-09-27 04:45:28 +0200

    Skynet Begins Here

---
+++ components/ChatBotCanvas.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ChatBotCanvas = () => {
+  return (
+    <div>ChatBotCanvas</div>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import { ChatBotCanvas } from "@/components/ChatBotCanvas";
+import { TextToSpeech } from "@/components/TextToSpeech";
+
+export default function Home() {
+  return (
+    <main className="h-screen">
+
+      <TextToSpeech />
+
+      <ChatBotCanvas />
+
+    </main>
+  )
+}
